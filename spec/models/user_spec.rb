ef69555c8f90666957a68609f8bb971ceefb730d
Jaime Jazareno <jaimejazareno1996@gmail.com>
2017-04-24 13:54:19 +0800

    added rspec to gemfile

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(fname: "Example User",lname: "Example User",username: "Example User",password: "Example User",password_confirmation: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:fname) }
+  it { should respond_to(:lname) }
+  it { should respond_to(:username) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:email) }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
 end
